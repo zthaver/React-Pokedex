59a5d1e90be5a6697dec4e1fbd8db2466c1b4352
Zain Thaver <47486742+zthaver@users.noreply.github.com>
2021-08-19 20:14:00 -0400

    got pokemon detail page working

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles'
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+import mockdata from './mockdata';
 import {
     AppBar,
     Toolbar,
     Grid,
+    Card,
+    CircularProgress,
+    CardMedia,
+    CardContent,
+    Typography
 } from '@material-ui/core'
 
 
@@ -12,26 +19,57 @@ pokedexContainer : {
     paddingTop: '20px',
     paddingLeft: '50px',
     paddingRight: '50px'
+},
+cardMedia:{
+    margin: "auto"
 }
 
 })
 
-const Pokedex = () => {
+
+
+const Pokedex = (props) => {
     const classes = useStyles();
-    return (
+    const [pokemonData,setPokemonData]= useState(mockdata);
+    const { history } = props;
+
+    const toFirstCharUppercase = name =>
+    name.charAt(0).toUpperCase() + name.slice(1);
+   
+    const getPokemonCard = (pokemonId) => {
+        const {id,name} = pokemonData[`${pokemonId}`];
+        const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            id
+          }.png`;
+        console.log(pokemonData[`${pokemonId}`])
+        return (
+            <Grid item xs={4} key={pokemonId}>
+                <Card onClick= {()=>history.push(`/${pokemonId}`)}>
+                    <CardMedia
+                    classes={classes.cardMedia}
+                    image={sprite}
+                    style={{width:"130px",height:"130px"}}
+                    />
+                    <CardContent> 
+                        <Typography> {`${id}.${toFirstCharUppercase(name)}`} </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        )
+    }
+        return (
         <>
         <AppBar position="static">
             <Toolbar/>
         </AppBar>
-        <Grid container spacing={2} className={classes.pokedexContainer} >
-            <Grid item xs={2}>
-                Item 1
-            </Grid>
-            <Grid item xs={2}>
-                Item 2
-            </Grid>
-
-        </Grid>
+        {pokemonData ?   <Grid container spacing={2} className={classes.pokedexContainer} >
+            {Object.keys(pokemonData).map((pokemonId)=>
+            getPokemonCard(pokemonId)
+            )}
+        </Grid>:
+        <CircularProgress/>
+        }
+      
         </>
     )
 }
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+
+import { Typography, Link } from "@material-ui/core";
+
+import { toFirstCharUppercase } from "./constants";
+
+import mockdata from "./mockdata";
+
 
 
 
@@ -6,8 +13,43 @@ const Pokemon = props =>{
     const { match } = props;
     const { params } = match;
     const { pokemonId } = params;
+    const [pokemon, setPokemon] = useState(mockdata[`${pokemonId}`]);
 
-    return <div> {`This is the pokemon page for pokemon # ${pokemonId}`} </div>
+
+    const generatePokemonJSX = () =>
+    {
+        const { name,id,species,height,weight,types,sprites } = pokemon;
+        const fullImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            id
+          }.png`;
+          const { front_default } = sprites;
+          return (
+            <>
+              <Typography variant="h1">
+                {`${id}.`} {toFirstCharUppercase(name)}
+                <img src={front_default} />
+              </Typography>
+              <img style={{ width: "300px", height: "300px" }} src={fullImageUrl} />
+              <Typography variant="h3">Pokemon Info</Typography>
+              <Typography>
+                {"Species: "}
+                <Link href={species.url}>{species.name} </Link>
+              </Typography>
+              <Typography>Height: {height} </Typography>
+              <Typography>Weight: {weight} </Typography>
+              <Typography variant="h6"> Types:</Typography>
+              {types.map((typeInfo) => {
+                const { type } = typeInfo;
+                const { name } = type;
+                return <Typography key={name}> {`${name}`}</Typography>;
+              })}
+            </>
+          );
+       
+
+        }
+    
+        return <> {generatePokemonJSX()} </>
 }
 
 
